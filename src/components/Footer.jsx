2c80bcb223125ff94df4b60bc9444c71fece5e18
Hamza <elonmusk0346@gmail.com>
2025-09-27 15:45:46 +0500

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => (
   <footer className="w-full text-center py-4 glass soft-shadow text-cyan-900 text-base fixed left-0 bottom-0 z-10">
-    <p>Made with ❤️ by <span className="text-cyan-400 font-semibold">Hamza</span> &copy; 2025</p>
+    <p>Made by <span className="text-cyan-400 font-semibold">Hamza</span> &copy; 2023</p>
   </footer>
 );
 
